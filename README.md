bd5933fd63ea87df405c3337ab92d3168948bdc5
Yaroslav Boiko <y.boikodevelop@gmail.com>
2019-06-13 12:08:46 +0300

    remove docker and change ui for material ui

--- README.md
+++ README.md
@@ -3,39 +3,14 @@
 #### Install dependencies
 
 ```
-yarn dc:install
+npm/yarn install
 ```
 
 ## Run
 
 #### Development
-
-Key `-d` will start in hidden mode
 ```
-docker-compose up mobile
+npm run start or yarn start
 ```
 
-or using `yarn`
-
-```
-yarn start
-```
-
-#### Installing npm dependencies
-
-All changes to `node_modules` should happen *inside* the containers. Install any new dependencies by inside the container. You can do this via `docker-compose run` or `yarn dc:install`, but it’s easier to just upadte a running container and avoid having to rebuild everything:
-
-```
-docker-compose run mobile yarn add <new_dependency>
-```
-### Deployment
-
-First of all create `.env` file from `.env.skel`
-
-Then install project dependencies:
-```
-docker-compose run mobile yarn
-```
-And then:
-```docker-compose up -d mobile```
 
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      marginBottom: theme.spacing(2)
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+  }));
+
+const Home = () => {
+    const classes = useStyles();
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box pt={20}>
+                <Link to="/login">
+                    <Button fullWidth variant="contained" color="primary" className={classes.button}>
+                        Login
+                    </Button>
+                </Link>
+                <Link to="/create-new-wallet">
+                    <Button fullWidth variant="contained" color="primary" className={classes.button}>
+                        Create new wallet
+                    </Button>
+                </Link>
+            </Box>
+
+            <Box className="home__bottom">
+                <Link to="/about">
+                    <Button fullWidth color="primary">
+                        About iOWN
+                    </Button>
+                </Link>
+                <Link to="/private-policy">
+                    <Button color="primary" className={classes.button}>
+                        Private Policy
+                    </Button>
+                </Link>
+                <Link to="/cookies-policy">
+                    <Button color="primary" className={classes.button}>
+                        Cookie Policy
+                    </Button>
+                </Link>
+            </Box>
+        </Container>
+        
+    );
+};
+
+export default Home;
